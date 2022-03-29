6dc3129aaabace287d20340eadcd868357c72669
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-03-29 21:50:42 +0500

    completed functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {FunctionalComp} from './components/FunctionalComp';
 
 function App() {
   return (
     <div className="App">
-     Na gap
+     <FunctionalComp/>
     </div>
   );
 }
---
+++ src/components/FunctionalComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// const FunctionalComp = () => {
+//   return (
+//     <div>FunctionalComp</div>
+//   )
+// }
+
+export const FunctionalComp=()=><div>FunctionalComp1</div>
+
+// export default FunctionalComp
