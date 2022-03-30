3842c9602c144586316206b9e0e290128099440e
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-03-30 23:47:33 +0500

    created class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ClassComponent from './components/ClassComponent';
 import { FunctionalComponent } from './components/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
      <FunctionalComponent/>
+     <ClassComponent/>
     </div>
   );
 }
---
+++ src/components/ClassComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ClassComponent extends Component {
+  render() {
+    return (
+      <div>Class Component</div>
+    )
+  }
+}
+
+export default ClassComponent
