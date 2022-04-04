3118c6ef1e7e7d5f679ef95bb7903dfcc2b99230
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-05 00:45:29 +0500

    welldone

--- src/jsx/Hello.js
+++ src/jsx/Hello.js
@@ -2,10 +2,14 @@ import React from "react";
 
 export const Hello = () => {
   // return (
-  //   <div>
+  //   <div className='dummyClass'>
   //       <h1>Sobirov Asadbek</h1>
   //   </div>
   // )
 
-  return React.createElement("div", null,React.createElement('h1',null,'Sobirov Asadbek'));
+  return React.createElement(
+    "div",
+    {id:'hello',className:'dummyClass'},
+    React.createElement("h1", null, "Sobirov Asadbek")
+  );
 };
