db60c9ae7f3103996435b373e83ce45388ba1f24
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-08 23:50:28 +0500

    destructure in classComp

--- src/props/Greet.js
+++ src/props/Greet.js
@@ -1,27 +1,28 @@
-import React from 'react'
+// import React from 'react'
 
-const Greet = ({name,lastName,children}) => {
-    console.log(name,lastName);
-  return (
-      <div>
-        <h1>Welcome {name} {lastName}  </h1>
-        {children}
-      </div>
-  )
-}
-
-export default Greet
-// import React, { Component } from 'react';
-
-// class Greet extends Component {
-//     render(props) {
-//         return (
-//             <div>
-//                 <h1>welcome {this.props.name}</h1>
-//                 {this.props.children}
-//             </div>
-//         );
-//     }
+// const Greet = props => {
+//    const {name,lastName,children}=props;
+//   return (
+//       <div>
+//         <h1>Welcome {name} {lastName}  </h1>
+//         {children}
+//       </div>
+//   )
 // }
 
-// export default Greet;
+// export default Greet
+import React, { Component } from 'react';
+
+class Greet extends Component {
+    render(props) {
+        const {name,children}=this.props;
+        return (
+            <div>
+                <h1>welcome {name}</h1>
+                {children}
+            </div>
+        );
+    }
+}
+
+export default Greet;
