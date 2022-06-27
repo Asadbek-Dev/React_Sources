3957bda0f77d9d2cc7d3cdbcef7c7a24896ffec3
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-06-27 23:37:01 +0500

    added components

---
+++ src/Context/ComponentC.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ComponentC() {
+  return (
+    <div>ComponentC</div>
+  )
+}
---
+++ src/Context/ComponentF.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ComponentF() {
+  return (
+    <div>ComponentF</div>
+  )
+}
+
+export default ComponentF
